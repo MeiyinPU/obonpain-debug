197fdd70aebd224a395c5e68263da5f8bcc4f145
myp <mypu@myyahoo.com>
2024-02-08 07:01:14 +0100

    add Footer Header PageAccueil

---
+++ obonpain/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.scss'
+import  PageAccueil  from './components/PageAccueil/PageAccueil.jsx'
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <PageAccueil />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ obonpain/src/components/PageAccueil/PageAccueil.jsx
@@ -0,0 +1,11 @@
+import Header from '../Header/Header.jsx'
+import Footer from '../Footer/Footer.jsx'
+
+export default function PageAccueil() {
+  return (
+    <div><Header />
+    <h1>Page Accueil</h1>
+    <Footer />
+    </div>
+  )
+}
